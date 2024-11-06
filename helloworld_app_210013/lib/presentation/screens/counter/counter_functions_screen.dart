3b196ff8075ad83e9071b6123110b367f196c1f9
AlexisG <210013@utxicotepec.edu.mx>
2024-11-05 18:14:58 -0600

    correccion de codigo

--- helloworld_app_210013/lib/presentation/screens/counter/counter_functions_screen.dart
+++ helloworld_app_210013/lib/presentation/screens/counter/counter_functions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CounterFunctionsScreen extends StatefulWidget {
   const CounterFunctionsScreen({super.key});
@@ -13,86 +14,93 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Contador-200995'),
-          actions: [
-            IconButton(
-                icon: const Icon(Icons.refresh_rounded),
-                onPressed: () {
-                  setState(() {
-                    clickCounter = 0;
-                  });
-                }),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('$clickCounter',
-                  style: const TextStyle(
-                      fontSize: 160, fontWeight: FontWeight.w100)),
-
-              Text('Click${clickCounter == 1 ? '' : 's'}',
-                  style: const TextStyle(fontSize: 25))
-
-              // if( clickCounter == 1 )
-              //   const Text('Click', style: TextStyle(fontSize: 25)),
-
-              // if( clickCounter != 1 )
-              //   const Text('Clicks', style: TextStyle(fontSize: 25)),
-            ],
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Counter Functions - 210013',
+            style: GoogleFonts.rubikWetPaint(), // Aplica la fuente al título del AppBar
           ),
         ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CustomButton(
-              icon: Icons.refresh_rounded,
-              onPressed: () {
-                clickCounter = 0;
-                setState(() {});
-              },
+            Text(
+              '$clickCounter',
+              style: GoogleFonts.rubikWetPaint(
+                fontSize: 160,
+                fontWeight: FontWeight.w300,
+                color: clickCounter > 0
+                    ? Colors.green
+                    : clickCounter < 0
+                        ? Colors.red
+                        : Colors.blue,
+              ),
             ),
-            const SizedBox(height: 10),
-            CustomButton(
-              icon: Icons.exposure_minus_1_outlined,
-              onPressed: () {
-                setState(() {});
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomButton(
-              icon: Icons.plus_one,
-              onPressed: () {
-                clickCounter++;
-                setState(() {});
-              },
+            Text(
+              (clickCounter == 1 || clickCounter == -1) ? 'Click' : 'Clicks',
+              style: GoogleFonts.rubikWetPaint( // Aplica la fuente al texto "Click/Clicks"
+                fontSize: 25,
+                fontWeight: FontWeight.w100,
+              ),
             ),
           ],
-        ));
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(
+            icon: Icons.plus_one,
+            color: Colors.green,
+            onPressed: () {
+              clickCounter++;
+              setState(() {});
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            icon: Icons.exposure_minus_1_outlined,
+            color: Colors.red,
+            onPressed: () {
+              clickCounter--;
+              setState(() {});
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            icon: Icons.refresh_rounded,
+            color: Colors.blue,
+            onPressed: () {
+              clickCounter = 0;
+              setState(() {});
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
 
 class CustomButton extends StatelessWidget {
   final IconData icon;
+  final Color color;
   final VoidCallback? onPressed;
 
   const CustomButton({
     super.key,
     required this.icon,
+    required this.color,
     this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      // shape: const StadiumBorder(),
-      enableFeedback: true,
-      elevation: 5,
       onPressed: onPressed,
+      backgroundColor: color,
       child: Icon(icon),
     );
   }
-} 
-
+}
